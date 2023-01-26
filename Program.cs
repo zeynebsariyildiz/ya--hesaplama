568854282f987493a2e92c2e72556cd1b8354757
Zeyneb Mehlika Sarıyıldız <sariyildiz.mehlika07@gmail.com>
2023-01-26 12:19:32 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yaş_hesaplama
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lütfen Doğum Yılınızı Giriniz");
+            Console.ReadLine();
+            DateTime dgunu = new  DateTime ();
+          DateTime simdi = DateTime.Today;
+          int yas =simdi.Year-dgunu.Year;
+            if (dgunu > simdi.AddYears(-yas))
+                yas--;
+            Console.WriteLine(yas);
+            Console.ReadLine();
+
+        }
+    }
+}
